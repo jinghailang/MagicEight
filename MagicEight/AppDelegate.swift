ee707ddb481be80b25c1d197c19973a10d816430
zengtao <895989112@qq.com>
2016-08-31 16:41:05 +0800

    Initial Commit

---
+++ MagicEight/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MagicEight
+//
+//  Created by litao on 16/8/31.
+//  Copyright © 2016年 zengtao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MagicEight/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MagicEight
+//
+//  Created by litao on 16/8/31.
+//  Copyright © 2016年 zengtao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
